0956b276498d2cb0c71f2ee2cdb8d1c601ab7663
Kirill Zdornyy <kirill@dineserve.com>
2016-03-10 20:41:46 -0800

    Add A-Z characters and spacebar to virtual keyboard.

--- src/main.rs
+++ src/main.rs
@@ -204,18 +204,47 @@ fn set_widgets(ui: &mut UiCell,
                clicked: &mut bool,
                submitted: &mut bool,
                input: &mut String) {
+    // let keys_first_row = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+    // let keys_second_row = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+    // let keys_third_row = ["Z", "X", "C", "V", "B", "N", "M"];
+
 
     // Unique ID's for each widget.
     widget_ids!{
         MASTER,
         COL,
         LABEL,
-        BUTTON,
-        BUTTON2,
         SUBMIT_BUTTON,
         TEXTBOX,
         BACKSPACE,
         INSTRUCTIONS_LABEL,
+        BUTTON_Q,
+        BUTTON_W,
+        BUTTON_E,
+        BUTTON_R,
+        BUTTON_T,
+        BUTTON_Y,
+        BUTTON_U,
+        BUTTON_I,
+        BUTTON_O,
+        BUTTON_P,
+        BUTTON_A,
+        BUTTON_S,
+        BUTTON_D,
+        BUTTON_F,
+        BUTTON_G,
+        BUTTON_H,
+        BUTTON_J,
+        BUTTON_K,
+        BUTTON_L,
+        BUTTON_Z,
+        BUTTON_X,
+        BUTTON_C,
+        BUTTON_V,
+        BUTTON_B,
+        BUTTON_N,
+        BUTTON_M,
+        BUTTON_SPACE
     }
 
     // Create the canvas.
@@ -233,7 +262,7 @@ fn set_widgets(ui: &mut UiCell,
         .line_spacing(2.5)
         .set(LABEL, ui);
 
-    Text::new("Enter your message in the green field and press Enter to send to \
+    Text::new("Type in your message and press Enter to send to \
                             everyone.")
         .color(color::BLUE)
         .padded_w_of(COL, PAD)
@@ -241,30 +270,6 @@ fn set_widgets(ui: &mut UiCell,
         .align_text_middle()
         .line_spacing(2.5)
         .set(INSTRUCTIONS_LABEL, ui);
-
-    Button::new()
-        .w_h(44.0, 44.0)
-        .mid_left_of(MASTER)
-        .rgb(0.4, 0.75, 0.6)
-        .label("A")
-        .react(|| {
-            let mut n = input.clone();
-            n.push('A');
-            *input = n;
-        })
-        .set(BUTTON, ui);
-
-    Button::new()
-        .w_h(44.0, 44.0)
-        .right_from(BUTTON, 20.0)
-        .rgb(0.4, 0.75, 0.6)
-        .label("B")
-        .react(|| {
-            let mut n = input.clone();
-            n.push('B');
-            *input = n;
-        })
-        .set(BUTTON2, ui);
 
     Button::new()
         .w_h(200.0, 44.0)
@@ -297,4 +302,368 @@ fn set_widgets(ui: &mut UiCell,
             *submitted = true;
         })
         .set(TEXTBOX, ui);
+
+    Button::new()
+        .w_h(200.0, 44.0)
+        .bottom_right_of(MASTER)
+        .rgb(0.4, 0.75, 0.6)
+        .label("Submit")
+        .react(|| {
+            *submitted = true;
+        })
+        .set(SUBMIT_BUTTON, ui);
+
+    let button_size = 44.0;
+    let button_padding = 5.0;
+
+    // NOTE: This is very much a prototype. Please don't judge.
+
+    Button::new()
+        .w_h(button_size, button_size)
+        .mid_left_of(MASTER)
+        .rgb(0.4, 0.75, 0.6)
+        .label("Q")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('Q');
+            *input = n;
+        })
+        .set(BUTTON_Q, ui);
+
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_Q, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("W")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('W');
+            *input = n;
+        })
+        .set(BUTTON_W, ui);
+
+    // E.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_W, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("E")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('E');
+            *input = n;
+        })
+        .set(BUTTON_E, ui);
+
+    // R.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_E, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("R")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('R');
+            *input = n;
+        })
+        .set(BUTTON_R, ui);
+
+    // T.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_R, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("T")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('T');
+            *input = n;
+        })
+        .set(BUTTON_T, ui);
+
+    // Y.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_T, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("Y")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('Y');
+            *input = n;
+        })
+        .set(BUTTON_Y, ui);
+
+    // U.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_Y, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("U")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('U');
+            *input = n;
+        })
+        .set(BUTTON_U, ui);
+
+    // I.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_U, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("I")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('I');
+            *input = n;
+        })
+        .set(BUTTON_I, ui);
+
+    // O.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_I, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("O")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('O');
+            *input = n;
+        })
+        .set(BUTTON_O, ui);
+
+    // P.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_O, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("P")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('P');
+            *input = n;
+        })
+        .set(BUTTON_P, ui);
+
+    // A.
+    Button::new()
+        .w_h(button_size, button_size)
+        .down_from(BUTTON_Q, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("A")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('A');
+            *input = n;
+        })
+        .set(BUTTON_A, ui);
+
+    // S.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_A, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("S")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('S');
+            *input = n;
+        })
+        .set(BUTTON_S, ui);
+
+    // D.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_S, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("D")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('D');
+            *input = n;
+        })
+        .set(BUTTON_D, ui);
+
+    // F.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_D, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("F")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('F');
+            *input = n;
+        })
+        .set(BUTTON_F, ui);
+
+    // G.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_F, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("G")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('G');
+            *input = n;
+        })
+        .set(BUTTON_G, ui);
+
+    // H.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_G, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("H")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('H');
+            *input = n;
+        })
+        .set(BUTTON_H, ui);
+
+    // J.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_H, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("J")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('J');
+            *input = n;
+        })
+        .set(BUTTON_J, ui);
+
+    // K.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_J, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("K")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('K');
+            *input = n;
+        })
+        .set(BUTTON_K, ui);
+
+    // L.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_K, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("L")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('L');
+            *input = n;
+        })
+        .set(BUTTON_L, ui);
+
+    // Z.
+    Button::new()
+        .w_h(button_size, button_size)
+        .down_from(BUTTON_A, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("Z")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('Z');
+            *input = n;
+        })
+        .set(BUTTON_Z, ui);
+
+    // X.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_Z, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("X")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('X');
+            *input = n;
+        })
+        .set(BUTTON_X, ui);
+
+    // C.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_X, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("C")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('C');
+            *input = n;
+        })
+        .set(BUTTON_C, ui);
+
+    // V.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_C, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("V")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('V');
+            *input = n;
+        })
+        .set(BUTTON_V, ui);
+
+    // B.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_V, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("V")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('B');
+            *input = n;
+        })
+        .set(BUTTON_B, ui);
+
+    // N.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_B, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("N")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('N');
+            *input = n;
+        })
+        .set(BUTTON_N, ui);
+
+    // M.
+    Button::new()
+        .w_h(button_size, button_size)
+        .right_from(BUTTON_N, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("M")
+        .react(|| {
+            let mut n = input.clone();
+            n.push('M');
+            *input = n;
+        })
+        .set(BUTTON_M, ui);
+
+    // Space.
+    Button::new()
+        .w_h(200.0, button_size)
+        .down_from(BUTTON_Z, button_padding)
+        .rgb(0.4, 0.75, 0.6)
+        .label("Space")
+        .react(|| {
+            let mut n = input.clone();
+            n.push(' ');
+            *input = n;
+        })
+        .set(BUTTON_SPACE, ui);
 }
